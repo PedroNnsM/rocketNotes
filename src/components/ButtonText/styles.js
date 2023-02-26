02c3afbdc33be41bafd7a9295e55a4c51df86500
PedroNnsM <pedronunes95@outlook.com>
2023-02-26 16:18:39 -0300

    Componente de ButtonText criado

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background: none ;
+  color: ${({ theme }) => theme.COLORS.ORANGE};
+  border: NONE;
+  font-size: 16px;
+
+`
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import { Container, Links } from "./styles.js";
 
 import { Button } from "../../components/Button/index.jsx";
+import { ButtonText } from "../../components/ButtonText/index.jsx";
 import { Header } from "../../components/Header/index.jsx";
 import { Section } from "../../components/Section/index.jsx";
 import { Tag } from "../../components/Tags/index.jsx";
@@ -9,6 +10,8 @@ export function Details() {
   return (
     <Container>
       <Header />
+
+      <ButtonText title='Excluir nota' />
 
       <Section title="Links úteis">
         <Links>
