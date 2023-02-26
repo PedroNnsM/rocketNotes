ecc9221d3fbf6e9a13e0a2c5dbb9b90556020ff2
PedroNnsM <pedronunes95@outlook.com>
2023-02-26 16:10:23 -0300

    Section marcadores + componente tags

---
+++ src/components/Tags/index.jsx
@@ -0,0 +1,7 @@
+import { Container } from "./styles";
+
+export function Tag({ title }) {
+  return (
+  <Container>{title}</Container>
+  );
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,24 +2,35 @@ import { Container, Links } from "./styles.js";
 
 import { Button } from "../../components/Button/index.jsx";
 import { Header } from "../../components/Header/index.jsx";
-import { Section } from "../../components/Section/index.jsx"; 
+import { Section } from "../../components/Section/index.jsx";
+import { Tag } from "../../components/Tags/index.jsx";
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title='Links úteis'>
+      <Section title="Links úteis">
         <Links>
           <li>
-            <a href="https://www.rockeseat.com.br">https://www.rockeseat.com.br</a>
+            <a href="https://www.rockeseat.com.br">
+              https://www.rockeseat.com.br
+            </a>
           </li>
           <li>
-          <a href="https://www.rockeseat.com.br">https://www.rockeseat.com.br</a>
+            <a href="https://www.rockeseat.com.br">
+              https://www.rockeseat.com.br
+            </a>
           </li>
-          
         </Links>
       </Section>
+
+      <Section title='Marcadores '>
+        <Tag title='React' />
+        <Tag title='NodeJS' />
+        <Tag title='Express' />
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   );
