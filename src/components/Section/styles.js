bbc4d1d5985c3321502f3571c788312c36158b66
PedroNnsM <pedronunes95@outlook.com>
2023-02-27 09:58:10 -0300

    ajustado o grid da home page

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  margin: 56px 0 28px;
+  margin:  28px 0;
 
   > h2 {
     border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
     padding-bottom: 16px;
-    margin-bottom: 24px;
+    margin-bottom: 28px;
 
     color: ${({ theme }) => theme.COLORS.GRAY_100};
     font-size: 20px;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyles from "./Styles/global";
 import { ThemeProvider } from "styled-components";
-import { Details } from "./pages/Details";
+import { Home } from "./pages/Home";
 
 import theme from "./styles/theme";
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 );
