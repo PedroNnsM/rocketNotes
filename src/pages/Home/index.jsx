b8c6b16c84db8a86738bf7abbaf0260a703281d6
PedroNnsM <pedronunes95@outlook.com>
2023-02-27 10:30:19 -0300

    botao criar nota ok

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,7 @@
+import {FiPlus } from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+
 
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
@@ -28,7 +31,10 @@ export function Home() {
 
       <Content></Content>
 
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -54,5 +54,13 @@ export const Content = styled.div`
 `;
 export const NewNote = styled.button`
   grid-area: newnote ;
+
+  background-color: ${({theme}) => theme.COLORS.ORANGE};
+  border: none;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
   
+  gap: 8px;
 `;
