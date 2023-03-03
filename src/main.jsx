035406621d584fce0846db86b5c8f8f16b096d82
PedroNnsM <pedronunes95@outlook.com>
2023-03-02 22:15:12 -0300

    Form login

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyles from "./Styles/global";
 import { ThemeProvider } from "styled-components";
-import { Home } from "./pages/Home";
+import { Signin } from "./pages/Signin";
 
 import theme from "./styles/theme";
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Signin />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,28 @@
+import { Container, Form } from "./styles";
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function Signin() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar links úteis</p>
+
+        <h2>Façca seu Login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">
+          Criar conta
+          </a>
+      </Form>
+    </Container>
+  );
+}
