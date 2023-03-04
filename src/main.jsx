e83a167ee9e8fa0d98c07637fa5050fabc6d4773
PedroNnsM <pedronunes95@outlook.com>
2023-03-03 23:10:04 -0300

    iniciando signUp

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyles from "./Styles/global";
 import { ThemeProvider } from "styled-components";
-import { Signin } from "./pages/Signin";
+import { SignUp } from "./pages/SignUp";
 
 import theme from "./styles/theme";
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Signin />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,29 @@
+import { Background, Container, Form } from "./styles";
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function SignUp() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar links úteis</p>
+
+        <h2>Façca seu Login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">
+          Criar conta
+          </a>
+      </Form>
+      <Background />
+    </Container>
+  );
+}
