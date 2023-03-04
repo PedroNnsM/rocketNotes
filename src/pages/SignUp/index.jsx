b9779466a0b1d96b36d11f5de5e2ac7017ade912
PedroNnsM <pedronunes95@outlook.com>
2023-03-03 23:14:55 -0300

    SignUp finalizado

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,5 @@
 import { Background, Container, Form } from "./styles";
-import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { FiLogIn, FiMail, FiLock, FiUser } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -7,23 +7,25 @@ import { Button } from "../../components/Button";
 export function SignUp() {
   return (
     <Container>
+      <Background />
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar links úteis</p>
 
-        <h2>Façca seu Login</h2>
+        <h2>Crie sua conta</h2>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
 
         <Input placeholder="E-mail" type="text" icon={FiMail} />
 
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button title="Entrar" />
+        <Button title="Cadastrar" />
 
         <a href="#">
-          Criar conta
+          Voltar para o login
           </a>
       </Form>
-      <Background />
     </Container>
   );
 }
