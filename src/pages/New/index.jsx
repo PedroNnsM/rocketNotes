6ec1ff810831b74b13564f9cedeaa6fa589660d1
PedroNnsM <pedronunes95@outlook.com>
2023-03-05 21:45:22 -0300

    Textareacriado

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,26 @@
-import { Header } from '../../components/Header'
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Textarea } from "../../components/Textarea";
 
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
-export function New(){
-  return(
+export function New() {
+  return (
     <Container>
       <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+
+          <Input placeholder='Titulo' />
+
+          <Textarea placeholder='Observações'/>
+        </Form>
+      </main>
     </Container>
-  )
+  );
 }
