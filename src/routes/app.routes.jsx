33ee409e41f1b855ef8a77f2b09eef6aff0313ac
PedroNnsM <pedronunes95@outlook.com>
2023-03-07 11:49:53 -0300

    git add .

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import { New } from "../pages/New";
+import { Home } from "../pages/Home";
+import { Profile } from "../pages/Profile";
+import { Details } from "../pages/New";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/details" element={<Details />} />
+    </Routes>
+  );
+}
