7657d59adc23327dbcfccb6673d7d10a922cdd4e
PedroNnsM <pedronunes95@outlook.com>
2023-03-23 13:22:58 -0300

    puxando os dados do cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,22 @@
-import { Background, Container, Form } from "./styles";
-import { FiLogIn, FiMail, FiLock, FiUser } from "react-icons/fi";
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { Link } from "react-router-dom";
+
+import { Background, Container, Form } from "./styles";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    console.log(name, email, password);
+  }
+
   return (
     <Container>
       <Background />
@@ -15,18 +26,30 @@ export function SignUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
+        />
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
+        />
 
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
-        <Link to='/' >
-          Voltar para o login
-        </Link>
-          
+        <Link to="/">Voltar para o login</Link>
       </Form>
     </Container>
   );
