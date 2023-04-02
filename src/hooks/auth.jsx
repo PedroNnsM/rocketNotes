697decd0f7959df838af22de43f40dbfb9ae022b
PedroNnsM <pedronunes95@outlook.com>
2023-04-01 23:15:47 -0300

    logando

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,14 +1,23 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 import { api } from "../services/api";
 
 const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState({});
+
+
   async function signIn({ email, password }) {
+
     try {
       const response = await api.post("/sessions", { email, password });
-      console.log(response);
+      const { user, token } = response.data;
+
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({ user, token });
+
+
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
@@ -19,7 +28,9 @@ function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
