a11ac520d418cd5014f635a7f86b26f6a070fbb8
PedroNnsM <pedronunes95@outlook.com>
2023-04-03 15:54:37 -0300

    usando o context

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
-  return (
-    <BrowserRouter>
-      {/* <AppRoutes /> */}
-      <AuthRoutes />
-    </BrowserRouter>
-  );
+  const { user } = useAuth();
+
+  return <BrowserRouter>{user ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>;
 }
