70276525f5b0644c841ecb45f7aaae84c6ec7db8
PedroNnsM <pedronunes95@outlook.com>
2023-04-05 21:35:12 -0300

    pegando os dados do profile com useState

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,21 @@
-import { Container, Form, Avatar } from "./styles";
-
+import { useState } from "react";
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
-import { Input } from "../../components/Input";
-import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
 
+import { useAuth } from "../../hooks/auth";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Avatar } from "./styles";
+
 export function Profile() {
+  const { user } = useAuth();
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
   return (
     <Container>
       <header>
@@ -23,10 +33,32 @@ export function Profile() {
             <input id="avatar" type="file" />
           </label>
         </Avatar>
-        <Input placegolder="Nome" type="text" icon={FiUser} />
-        <Input placegolder="E-Mail" type="text" icon={FiMail} />
-        <Input placegolder="Senha atual" type="password" icon={FiLock} />
-        <Input placegolder="Nova senha" type="password" icon={FiLock} />
+        <Input
+          placegolder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placegolder="E-Mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placegolder="Senha atual"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPasswordOld(e.target.value)}
+        />
+        <Input
+          placegolder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPasswordNew(e.target.value)}
+        />
         <Button title="Salvar" />
       </Form>
     </Container>
