8d3a45ffe4e690cb5ed61dafa41ca7036b350e3b
PedroNnsM <pedronunes95@outlook.com>
2023-04-05 21:24:43 -0300

    ajustando o axios

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -15,7 +15,8 @@ function AuthProvider({ children }) {
       localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
       localStorage.setItem("@rocketnotes:token", token);
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
       setData({ user, token });
     } catch (error) {
       if (error.response) {
@@ -38,7 +39,8 @@ function AuthProvider({ children }) {
     const user = localStorage.getItem("@rocketnotes:user");
 
     if (token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      
       setData({
         token,
         user: JSON.parse(user),
