a2266fb1eb59c089c6f9fe8a7bf840094faabad8
PedroNnsM <pedronunes95@outlook.com>
2023-04-07 12:15:28 -0300

    add link

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -7,8 +7,17 @@ import { Button } from "../../components/Button";
 
 import { Container, Form } from "./styles";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  function handleAddLink() {
+    setLinks((prevState) => [...prevState, newLink]);
+    setNewLink("");
+  }
+
   return (
     <Container>
       <Header />
@@ -25,8 +34,16 @@ export function New() {
           <Textarea placeholder="Observações" />
 
           <Section title="Links úteis">
-            <NoteItem value="https://github.com" />
-            <NoteItem isNew placeholder="Novo Link" />
+            {links.map((link, index) => (
+              <NoteItem key={String(index)} value={link} onClick={() => {}} />
+            ))}
+            <NoteItem
+              isNew
+              placeholder="Novo Link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
