14f8c0f89ebd2bea91f3877c9be9d12734fff18f
PedroNnsM <pedronunes95@outlook.com>
2023-04-15 23:53:33 -0300

    erro surgindo nas notas

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,54 +1,63 @@
+import { useEffect, useState } from "react";
 import { Container, Links, Content } from "./styles.js";
+import { useParams } from "react-router-dom";
 
 import { Button } from "../../components/Button/index.jsx";
 import { ButtonText } from "../../components/ButtonText/index.jsx";
 import { Header } from "../../components/Header/index.jsx";
 import { Section } from "../../components/Section/index.jsx";
 import { Tag } from "../../components/Tags/index.jsx";
+import { api } from "../../services/api.js";
 
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      console.log(response.data)
+      setData(response.data);
+
+    }
+    fetchNote();
+    
+  }, []);
   return (
     <Container>
       <Header />
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
 
-          <h1>Introdução ao React</h1>
+      {data && 
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" />
 
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, nam
-            possimus, voluptates consectetur itaque similique vero corporis
-            veniam sapiente pariatur explicabo ipsam consequatur culpa eum unde
-            expedita? Hic, minima rem!Lorem ipsum, dolor sit amet consectetur
-            adipisicing elit. Ipsa, nam possimus, voluptates consectetur itaque
-            similique vero corporis veniam sapiente pariatur explicabo ipsam
-            consequatur culpa eum unde expedita? Hic, minima rem!
-          </p>
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="https://www.rockeseat.com.br">
-                  https://www.rockeseat.com.br
-                </a>
-              </li>
-              <li>
-                <a href="https://www.rockeseat.com.br">
-                  https://www.rockeseat.com.br
-                </a>
-              </li>
-            </Links>
-          </Section>
+            <h1>{data.title}</h1>
 
-          <Section title="Marcadores ">
-            <Tag title="React" />
-            <Tag title="NodeJS" />
-            <Tag title="Express" />
-          </Section>
-
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <p>{data.description}</p>
+            {data.links && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a href={link.url}>{link.url}</a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
+            {data.tags && (
+              <Section title="Marcadores ">
+                {data.tag.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
+            <Button title="Voltar" />
+          </Content>
+        </main>
+      }
     </Container>
   );
 }
