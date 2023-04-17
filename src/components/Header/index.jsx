b1c510d4631c1759c950ba870a082bfb2f74dfec
PedroNnsM <pedronunes95@outlook.com>
2023-04-17 17:42:05 -0300

    ajuste no signOut

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,16 @@ import { Container, Profile, Logout } from "./styles";
 import { RiShutDownLine } from "react-icons/ri";
 import { api } from "../../services/api";
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
   const { signOut, user } = useAuth();
+  const navigation = useNavigate()
+
+  function handleSignOut(){
+    navigation('/')
+    signOut()
+  }
 
   const avatarUrl = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
@@ -22,9 +29,10 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
   );
 }
+0
