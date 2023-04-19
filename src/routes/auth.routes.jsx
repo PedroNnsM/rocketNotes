d08c036d86625b578dec95febe3a13bb91388e5e
PedroNnsM <pedronunes95@outlook.com>
2023-04-19 17:09:31 -0300

    ajuste 1

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
-import { SignIn } from "../pages/SignIn";
+
 import { SignUp } from "../pages/SignUp";
+import { SignIn } from "../pages/SignIn";
 
 export function AuthRoutes() {
   return (
